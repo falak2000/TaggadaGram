4e5232e6d2807facd7d451bd41b807aa27f05cfb
Falak Sharma <falak.sharma@c02zf09xnqmd.corp.ola.com>
2022-07-26 14:16:23 +0530

    Entites package added

--- backend/src/main/java/TaggadaGramApplication.java
+++ backend/src/main/java/TaggadaGramApplication.java
@@ -1,2 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TaggadaGramApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaggadaGramApplication.class);
+    }
 }
